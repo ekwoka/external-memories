e1d31361b9010efbc80da417a3b797a44c8377cf
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2020-09-12 01:38:35 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -5,8 +5,7 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-const pluginRespimg = require( "eleventy-plugin-respimg" );
-​const​ ​lazyImagesPlugin​ ​=​ ​require​(​'eleventy-plugin-lazyimages'​)​;​
+const pluginRespimg = require( "eleventy-plugin-respimg" );​
 
 
 
@@ -14,7 +13,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
-  ​eleventyConfig​.​addPlugin​(​lazyImagesPlugin);
   
   eleventyConfig.addShortcode('respimg', (path, alt, sizes) => {
       const fetchBase = `https://res.cloudinary.com/${eleventyConfig.cloudinaryCloudName}/image/fetch/`;
