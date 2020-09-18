27bf6b6899c286d5c19db737d853646375ad9745
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2020-09-18 23:22:20 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -62,6 +62,7 @@ module.exports = function(eleventyConfig) {
             case "nav":
             case "post":
             case "posts":
+			case "wiki":
               return false;
           }
 
