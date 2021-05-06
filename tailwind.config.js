f50e411d2842e372ad03a3cd580fbb73ca3e737d
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-05-06 18:35:20 +0800

    Update Tailwind Mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const height = require("tailwindcss/defaultTheme")
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  mode: 'jit',
   important: false,
   purge: {
     mode: "all",
